2a2671840e2c011260d3d3a6cb9e175f858e1836
GracyDinma <okohagchidinma@gmail.com>
2025-11-04 09:25:00 -0600

    Added joins queries and readme files

---
+++ database-adv-script/README.md
@@ -0,0 +1,9 @@
+# ALX Airbnb Database ModuleUleashing Advanced Querying Power
+This project geared at implementing advanced SQL querying and optimization techniques to work with a simulated Airbnb database
+- Creatio of Entity Relationships Diagram
+- Complex Queries with Joins
+- The power of Subqueries
+- Aggregations and Windows Functions
+- Indexing for Optimization
+- Partitioning Large Tables
+- Performance Monitoring and Schema Refinement.
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,20 @@
+** Writing a query using an INNER JOIN to retrieve all bookings and the respective users who made those bookings.
+
+SELECT *
+FROM Booking
+INNER JOIN User
+ON Booking.user_id = User.user_id;
+
+## Writing a query using a LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews
+
+SELECT *
+FROM Property
+LEFT JOIN Review
+ON Review.property_id = Property.property_id;
+
+## Write a query using FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not to a user.
+
+SELECT *
+FROM User
+FULL OUTER JOIN Booking
+ON Booking.user_id = User.user_id;
