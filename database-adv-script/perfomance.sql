d7e4ec29782d438d9a26056c3e55c6eabef73db0
GracyDinma <okohagchidinma@gmail.com>
2025-11-06 14:46:01 -0600

    updated

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,9 +1,13 @@
 -- initial query
 SELECT *
 FROM Booking
-JOIN User ON Booking.user_id = User.user_id
-JOIN Property ON Booking.property_id = Property.property_id
-JOIN Payment ON Booking.booking_id = Payment.booking_id;
+JOIN User 
+  ON Booking.user_id = User.user_id
+  AND User.role = 'guest'
+JOIN Property 
+  ON Booking.property_id = Property.property_id
+JOIN Payment 
+  ON Booking.booking_id = Payment.booking_id;
 
 -- Analyze Performance
 EXPLAIN ANALYZE
@@ -18,6 +22,19 @@ CREATE INDEX idx_booking_property_id ON Booking(property_id);
 CREATE INDEX idx_payment_booking_id ON Payment(booking_id);
 
 -- Refactored Query
+SELECT
+    b.booking_id, b.status. b.start_date. b.end_date,
+    u.first_name, u.last_name, u_email,
+    p.title AS property_title, p.location,
+    pay.amount, pay.payment_status
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'active';
+
+-- Re-Analyzing Optimized Query
+EXPLAIN ANALYZE
 SELECT
     b.booking_id, b.status. b.start_date. b.end_date,
     u.first_name, u.last_name, u_email,
